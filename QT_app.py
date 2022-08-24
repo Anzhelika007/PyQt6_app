09ae735175820b49e9297612d8b7531bce53ee55
Anzhalika Turlak <293852647@mail.ru>
2022-08-24 15:57:50 +0300

    add sql class Interaction

--- QT_app.py
+++ QT_app.py
@@ -2,6 +2,10 @@ import sys
 from PyQt6.QtWidgets import QApplication, QDialog
 from ui_imagedialog import Ui_MainWindow
 
+from Base_SQLite import Interaction
+
+
+
 app = QApplication(sys.argv)
 window = QDialog()
 ui = Ui_MainWindow()
@@ -9,3 +13,4 @@ ui.setupUi(window)
 
 window.show()
 sys.exit(app.exec())
+
