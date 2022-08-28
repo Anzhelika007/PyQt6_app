45ef3ebcefd37f109e272947d63f36c94da01944
Anzhalika Turlak <293852647@mail.ru>
2022-08-28 22:08:28 +0300

    add func reg and table so-so

--- Base_SQLite.py
+++ Base_SQLite.py
@@ -1,40 +1,25 @@
 import sqlite3
 
+# --------------------------------------------------
+# Создали таблицу БД
+# --------------------------------------------------
 with sqlite3.connect('database.db') as db:
     cursor = db.cursor()
 
     cursor.execute("""CREATE TABLE IF NOT EXISTS users(
-    ID INTEGER PRIMARY KEY AUTOINCREMENT,
-    Username VARCHAR(30),   
+    Username_id VARCHAR(30),   
     Email VARCHAR(30),
     PhoneNumber VARCHAR(15)
     )""")
 
-    cursor.execute(
-        "INSERT INTO users VALUES ('Yanitya','geffelleceffe-7860@yopmail.com','9(3484)364-32-92'), ('Ricahav','kajaddunneito-2898@yopmail.com','5(4119)578-75-50'), ('Corbert','tebrijofuyu-9922@yopmail.com','4(04)160-64-89'), ('Xerxesar','houcrukefamu-5882@yopmail.com','152(238)276-36-46')")
+    users_x = [('Yanitya', 'geffelleceffe-7860@yopmail.com', '9(3484)364-32-92'),
+               ('Ricahav', 'kajaddunneito-2898@yopmail.com', '5(4119)578-75-50'),
+               ('Corbert', 'tebrijofuyu-9922@yopmail.com', '4(04)160-64-89'),
+               ('Xerxesar', 'houcrukefamu-5882@yopmail.com', '152(238)276-36-46')]
 
-    # cursor.execute('''SELECT * FROM users''')
-    # print(cursor.fetchall())
+    cursor.executemany("INSERT INTO users(Username_id, Email, PhoneNumber) VALUES (?,?,?)", users_x)
 
+    for i in cursor.execute('SELECT * FROM users'):
+        print(i)
 
-class Interaction:
-    def __init__(self):
-        # подключились к базе
-        self.connection = sqlite3.connect('database.db')
-
-    def add_user(self, Username, Email, PhoneNumber):
-        # записываем нового юзера
-        cursor = self.connection.cursor()
-        bd = ("""INSERT INTO users(Username, Email, PhoneNumber) VALUES (?,?,?)""", a, b, c)
-        cursor.execute(bd)
-        self.connection.commit()
-        cursor.close()
-
-    def show_users(self):
-        # показать всех
-        cursor = self.connection.cursor()
-        bd = ("""SELECT * FROM users""")
-        cursor.execute(bd)
-        show_all = cursor.fetchall()
-        return show_all
-        cursor.close()
+#     cursor.execute('''DELETE FROM users''')
--- main.py
+++ main.py
@@ -1,71 +1,64 @@
-from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication
+# Для исполнения команд строки
+import sys
 import sqlite3
-
-# --------------------------------------------------
-# Создали таблицу БД
-#--------------------------------------------------
-with sqlite3.connect('database.db') as db:
-    cursor = db.cursor()
-
-    cursor.execute("""CREATE TABLE IF NOT EXISTS users(
-    ID INTEGER PRIMARY KEY AUTOINCREMENT,
-    Username VARCHAR(30),   
-    Email VARCHAR(30),
-    PhoneNumber VARCHAR(15)
-    )""")
-
-    cursor.execute(
-        "INSERT INTO users VALUES ('Yanitya','geffelleceffe-7860@yopmail.com','9(3484)364-32-92'), ('Ricahav','kajaddunneito-2898@yopmail.com','5(4119)578-75-50'), ('Corbert','tebrijofuyu-9922@yopmail.com','4(04)160-64-89'), ('Xerxesar','houcrukefamu-5882@yopmail.com','152(238)276-36-46')")
-
-    # cursor.execute('''SELECT * FROM users''')
-    # print(cursor.fetchall())
-
-Form, Window = uic.loadUiType('interface.ui')
-app = QApplication([])
-window = Window()
-form = Form()
-form.setupUi(window)
-window.show()
-
-# def show_tabl():
+# Для динамической конвертации файла
+from PyQt5 import uic
+# Импортируем все виджеты, которые собираемся использовать в работе
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog, QWidget, QHeaderView
+from PyQt5 import QtWidgets
 
 
-def registration():
-    # считываем данные с полей и записываем в кортеж
-    user_login = form.userName.text()
-    if len(user_login) == 0:
-        print('Имя пользователя отсутствует!')
-
-    user_email = form.email.text()
-    if user_email == 0 and user_login not in '@':
-        print('Неверная почта или ее не ввели')
-
-    user_phone = form.phoneNamber.text()
-    if len(user_phone) < 7:
-        print('слишком короткий номер')
-
-    user_value = (user_login, user_email, user_phone)
-    print(user_login, user_email, user_phone, user_value)
-
-    # очищаем поля
-    form.form.userName.clear()
-    form.email.clear()
-    form.phoneNamber.clear()
-
-    return user_value
+# Создали класс и наследуем главное окно
 
 
-# def add_user(user_value):
-#     # записываем нового юзера
-#     cursor = cconnection.cursor('database.db')
-#     bd = ("""INSERT INTO users(Username, Email, PhoneNumber) VALUES (?,?,?)""", a, b, c)
-#     cursor.execute(bd)
-#     self.connection.commit()
-#     cursor.close()
+class UI(QMainWindow):
+    def __init__(self):
+        super(UI, self).__init__()
+
+        # load the ui file
+        uic.loadUi("interface.ui", self)
+        # Манипуляции
+        self.label_11.setText('erthtzrjnsymzgs')
+        # вызвали функцию показа таблицы
+        self.load_db()
+        # self.addUserBtn.clicked.connect(self.regist)
+
+        self.addUserBtn.clicked.connect(self.reg)
+
+    def reg(self):
+        self.user_login = self.userName.text()
+        self.user_email = self.email.text()
+        self.user_phone = self.phoneNamber.text()
+        self.user_value = (self.user_login, self.user_email, self.user_phone)
+        print(self.user_value)
+
+        self.conn = sqlite3.connect('database.db')
+        cursor = self.conn.cursor()
+        cursor.execute("INSERT INTO users(Username_id, Email, PhoneNumber) VALUES (?,?,?)", self.user_value)
+        self.conn.commit()
+        cursor.close()
+
+    def load_db(self):
+        self.conn = sqlite3.connect('database.db')
+        cursor = self.conn.cursor()
+        self.show_users = 'SELECT * FROM users'
+
+        # кол-во строк
+        self.tableWidget.setRowCount(10)
+
+        self.table_row = 0
+        for row in cursor.execute(self.show_users):
+            self.tableWidget.setItem(self.table_row, 0, QtWidgets.QTableWidgetItem(row[0]))
+            self.tableWidget.setItem(self.table_row, 1, QtWidgets.QTableWidgetItem(row[1]))
+            self.tableWidget.setItem(self.table_row, 2, QtWidgets.QTableWidgetItem(row[2]))
+            self.table_row = self.table_row + 1
+        cursor.close()
+
+        # show the app
+        self.show()
 
 
-
-form.addUserBtn.clicked.connect(registration)
-
+# Initialize The App
+app = QApplication(sys.argv)
+UIWindow = UI()
 app.exec_()
